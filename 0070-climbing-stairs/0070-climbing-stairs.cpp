2fdb71049838ffb7dec263a8b814c724fba79648
Mohammad Moin <66841704+moinymoh@users.noreply.github.com>
2023-10-08 18:46:37 +0530

    Time: 2 ms (47.69%), Space: 6.6 MB (5.54%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
-    int solve(int index, int n, vector<int> &dp) {
+    int solve(int index, int n, vector<int>& dp) {
         if(index > n) {
             return 0;
         }
-        
         if(index == n) {
             return 1;
         }
@@ -12,15 +11,14 @@ public:
         if(dp[index] != -1) {
             return dp[index];
         }
-        
         int oneStep = solve(index + 1, n, dp);
         int twoStep = solve(index + 2, n, dp);
         
         return dp[index] = oneStep + twoStep;
     }
     int climbStairs(int n) {
+        int index = 0;
         vector<int> dp(n + 1, -1);
-        return solve(0, n, dp);
-        
+        return solve(index, n, dp);
     }
 };
