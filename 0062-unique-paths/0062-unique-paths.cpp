eea880f110d7bf71575a76cfc4466b49647440c2
Mohammad Moin <66841704+moinymoh@users.noreply.github.com>
2023-10-11 01:31:10 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (7.04%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int solve(int i, int j, int m, int n, vector<vector<int>>& dp){
-        if(i >= m || j >= n){
+    int solve(int i, int j, int m, int n, vector<vector<int>>& dp) {
+        if(i >= m || j >= n) {
             return 0;
         }
-        if(i == m - 1 && j == n - 1){
+        if(i == m -1 && j == n - 1) {
             return 1;
         }
         
-        if(dp[i][j] != -1){
+        if(dp[i][j] != -1) {
             return dp[i][j];
         }
-        int right = solve(i + 1, j, m, n, dp);
-        int down = solve(i, j + 1, m, n, dp);
+        int right = solve(i, j + 1, m, n, dp);
+        int down = solve(i + 1, j, m, n, dp);
         
         return dp[i][j] = right + down;
     }
