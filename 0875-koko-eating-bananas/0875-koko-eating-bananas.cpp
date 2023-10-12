37c02aa6eeb45ce956a6fad497daa4bf80b5d331
Mohammad Moin <66841704+moinymoh@users.noreply.github.com>
2023-10-13 02:34:41 +0530

    Time: 46 ms (17.92%), Space: 19.4 MB (22.00%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool solve(vector<int>& piles, int h, int bananaAte) {
+        long long hoursTaken = 0;
+        // cout<< bananaAte;
+        for(int i = 0; i < piles.size(); i++) {
+            hoursTaken += ceil((double)piles[i] / (double)bananaAte);
+            // cout << hoursTaken<< endl;
+        }
+         // cout<< endl;
+        return hoursTaken <= h ? true: false;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low = 1;
+        int high = *max_element(piles.begin(), piles.end());
+        int ans = 0;
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if(solve(piles, h, mid)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        
+        return ans;
+    }
+};
